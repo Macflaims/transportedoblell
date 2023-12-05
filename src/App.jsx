ff3dc72df8f561d278635843e35217ba6ba8dd65
Macflaims <martinsanchez2533@gmail.com>
2023-12-05 15:18:23 -0300

    upload code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBarComponent from "./components/NavBar/NavBarComponent";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ImageWithText from "./components/Home/ImageWithText"
+import CardsSectionComponent from "./components/CardsSection/CardsSectionComponent";
+import SectionTitleComponent from "./components/SectionTitle/SectionTitleComponent";
+import ThreeStripesSection from "./components/ThreeStripes/ThreeStripesSection";
+import vehicles from "./assets/data/vehicles.json"
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+  return <div>
+    <NavBarComponent />
+    <ImageWithText
+      title="Transporte DobleLL"
+      text="Texto debajo del título" />
+    <SectionTitleComponent title="Por qué elegirnos?" />
+    <ThreeStripesSection />
+    <SectionTitleComponent title="Conoce nuestra flota" />
+    <CardsSectionComponent data={vehicles} />
+  </div>;
+
 }
 
-export default App
+
+
+export default App;
---
+++ src/components/Brand/BrandComponent.jsx
@@ -0,0 +1,14 @@
+import Navbar from 'react-bootstrap/Navbar';
+import "./BrandComponent.css"
+
+const BrandComponent = ()=> {
+    return(
+        <>
+            <Navbar.Brand href="#home" className="text-light"> Transportes </Navbar.Brand>        
+        </>
+    )
+}
+
+export default BrandComponent;
+
+
